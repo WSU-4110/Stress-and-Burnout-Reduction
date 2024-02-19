3fa215a7f4f43b09e21408c0685ca6ec892d597b
Carlos Nunez <carlosallen105@gmail.com>
2024-02-18 20:26:17 -0500

    Cleaning up code

--- videopage.js
+++ videopage.js
@@ -3,28 +3,25 @@ document.addEventListener('DOMContentLoaded', function() {
     const span = document.getElementsByClassName("close")[0];
     const videoFrame = document.getElementById("videoFrame");
 
-    const videos = document.getElementsByClassName("video-card");
-    Array.from(videos).forEach(function(video) {
+    document.querySelectorAll(".video-card").forEach(function(video) {
         video.addEventListener('click', function(e) {
             e.preventDefault();
             const videoUrl = this.getAttribute("data-video-url");
             const embedUrl = videoUrl.replace("watch?v=", "embed/");
-            videoFrame.src = embedUrl + "?autoplay=1&rel=0"; // Set the src and enable autoplay
+            videoFrame.src = `${embedUrl}?autoplay=1&rel=0`;
             modal.style.display = "block";
         });
     });
 
-    // Close
     span.onclick = function() {
         modal.style.display = "none";
-        videoFrame.src = ""; // Stop the video
+        videoFrame.src = "";
     }
 
-    // Close the modal when clicking outside of it
     window.onclick = function(event) {
-        if (event.target == modal) {
+        if (event.target === modal) {
             modal.style.display = "none";
-            videoFrame.src = ""; // Stop the video
+            videoFrame.src = "";
         }
     }
 });
