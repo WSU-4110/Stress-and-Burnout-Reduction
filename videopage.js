f578fd3155b517ac575c8462622b0f88b05ca1fa
Carlos Nunez <carlosallen105@gmail.com>
2024-02-19 20:40:34 -0500

    Adding comments

--- videopage.js
+++ videopage.js
@@ -1,25 +1,36 @@
+// Waits until DOM is loaded before running script
 document.addEventListener('DOMContentLoaded', function() {
+    // Retrieves modal element using its ID
     const modal = document.getElementById("modal");
     const span = document.getElementsByClassName("close")[0];
+    // Retrieves iframe element where the video will be embedded
     const videoFrame = document.getElementById("videoFrame");
 
+    // Attaches click event listeners to elements with video-card class
     document.querySelectorAll(".video-card").forEach(function(video) {
         video.addEventListener('click', function(e) {
             e.preventDefault();
             const videoUrl = this.getAttribute("data-video-url");
+            // Transforms video URL into proper embed URL and autoplay
             const embedUrl = videoUrl.replace("watch?v=", "embed/");
             videoFrame.src = `${embedUrl}?autoplay=1&rel=0`;
+            // Displays modal
             modal.style.display = "block";
         });
     });
 
+    // Onclick event listener attached to close button
     span.onclick = function() {
+        // Hides the modal
         modal.style.display = "none";
         videoFrame.src = "";
     }
-
+    
+    // Onclick event listener attached to window
     window.onclick = function(event) {
+        // Checks if click event is outside video player
         if (event.target === modal) {
+            // Hides the modal and clears video src
             modal.style.display = "none";
             videoFrame.src = "";
         }
