5d07eef600da5dd3d4888a22dc7c752daa4b2621
Noah Mousseau <112453632+NoahMousseau@users.noreply.github.com>
2024-03-05 22:19:59 -0500

    Testing reversion to old variant of project

--- functions/forum.js
+++ functions/forum.js
@@ -1,15 +1,39 @@
 import { v4 as uuidv4 } from 'uuid';
 
+// Function for using getAllPosts
+export async function getAllPosts({ env }) {
+  const allKeys = await env.FORUM.list();
+  const allPosts = [];
+ 
+  for (const key of allKeys.keys) {
+     const post = await env.FORUM.get(key.name, { type: 'json' });
+     allPosts.push(post);
+  }
+ 
+  return allPosts;
+ }
+
 export async function onRequestPost({ request, env }) {
  try {
+    // Form data getting
+    const formData = await request.formData();
+    const postTitle = formData.get('postTitle');
+    const postContent = formData.get('postContent');
+
+    // Create a post object
+    const post = JSON.stringify({
+      title: postTitle,
+      content: postContent,
+    });
 
     // Generate a unique ID for the post
     const uniqueId = uuidv4();
 
-    console.log("Post submitted successfully");
+    // Store the post in the KV namespace
+    await env.COOLFROG_FORUM.put(uniqueId, post);
 
   } catch (error) {
+    // Error handling
     console.error("Error:", error);
-    console.error("Stack trace:", error.stack);
   }
 };
