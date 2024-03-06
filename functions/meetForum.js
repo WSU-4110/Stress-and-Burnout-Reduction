7cea8bc2ec4297f1e2bb9737655f1dfdcaa1adec
Noah Mousseau <112453632+NoahMousseau@users.noreply.github.com>
2024-03-05 21:31:33 -0500

    Trying to fix namespace issue

--- functions/meetForum.js
+++ functions/meetForum.js
@@ -3,7 +3,7 @@ import { v4 as uuidv4 } from 'uuid';
 export async function onRequestPost({ request, env }) {
  try {
     // Assuming the request body contains the form data
-    const formData = await request.postForm();
+    const formData = await request.formData();
     const postLocation = formData.get('postTitle');
     const postMeetDetails = formData.get('postContent');
 
