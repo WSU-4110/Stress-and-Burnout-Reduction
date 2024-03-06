a971d32bb1b409f49e59e65c5475a951cecd83a7
Noah Mousseau <112453632+NoahMousseau@users.noreply.github.com>
2024-03-05 21:45:08 -0500

    Trying to stop page redirects

--- functions/meetForum.js
+++ functions/meetForum.js
@@ -1,26 +1,27 @@
-import { v4 as uuidv4 } from 'uuid';
-
 export async function onRequestPost({ request, env }) {
- try {
-    // Assuming the request body contains the form data
-    const formData = await request.formData();
-    const postLocation = formData.get('postTitle');
-    const postMeetDetails = formData.get('postContent');
+    try {
+        // Capture form data
+        const formData = await request.formData();
+        const postTitle = formData.get('postTitle');
+        const postContent = formData.get('postContent');
 
-    // Create a post object
-    const post = JSON.stringify({
-      location: postLocation,
-      meetDetails: postMeetDetails,
-    });
+        // Create a post object
+        const post = JSON.stringify({
+            title: postTitle,
+            content: postContent,
+        });
 
-    // Generate a unique ID for the post
-    const uniqueId = uuidv4();
+        // Generate a unique ID for the post
+        const uniqueId = uuidv4();
 
-    // Store the post in the KV namespace
-    await env.COOLFROG_MEETFORUM.put(uniqueId, post);
+        // Store the post in the KV namespace
+        await env.COOLFROG_MEETFORUM.put(uniqueId, post);
 
-  } catch (error) {
-    console.error("Error:", error);
-    // Error handling
-  }
+        console.log("Post submitted successfully");
+
+    } catch (error) {
+        //Handle errors
+        console.error("Error:", error);
+        console.log(error);
+    }
 };
