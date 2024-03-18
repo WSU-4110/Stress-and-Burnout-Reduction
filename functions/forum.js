9b32d192883fb87e6f65f3b32d5eca1955a20180
Noah Mousseau <112453632+NoahMousseau@users.noreply.github.com>
2024-03-18 17:49:54 -0400

    Trying to redirect user

--- functions/forum.js
+++ functions/forum.js
@@ -1,8 +1,21 @@
 import { v4 as uuidv4 } from 'uuid';
 
- export async function onRequestPost({ request, env }) {
+// Function for using getAllPosts
+export async function getAllPosts({ env }) {
+  const allKeys = await env.COOLFROG_FORUM.list();
+  const allPosts = [];
+
+  for (const key of allKeys.keys) {
+    const post = await env.COOLFROG_FORUM.get(key.name, { type: 'json' });
+    allPosts.push(post);
+  }
+
+  return allPosts;
+};
+
+export async function onRequestPost({ request, env }) {
   try {
-     // Assuming the request body contains the form data
+     // Request body contains the form data
      const formData = await request.formData();
      const postTitle = formData.get('postTitle');
      const postContent = formData.get('postContent');
@@ -18,22 +31,16 @@ import { v4 as uuidv4 } from 'uuid';
  
      // Store the post in the KV namespace
      await env.COOLFROG_FORUM.put(uniqueId, post);
- 
-   } catch (error) {
+
+     // After storing the post, redirect to the homepage
+    return new Response('Post created successfully!', {
+      status: 302,
+      headers: {
+        location: '/forum.html'
+      },
+    });
+  } catch (error) {
      console.error("Error:", error);
      // Error handling
-   }
- 
-   // Function for using getAllPosts
-  async function getAllPosts({ env }) {
-    const allKeys = await env.COOLFROG_FORUM.list();
-    const allPosts = [];
-  
-    for (const key of allKeys.keys) {
-      const post = await env.COOLFROG_FORUM.get(key.name, { type: 'json' });
-      allPosts.push(post);
-    }
-  
-    return allPosts;
-  };
- };
+  }
+};
