7778421eb0164707409b02a3f300e2f2b616717e
Noah Mousseau <112453632+NoahMousseau@users.noreply.github.com>
2024-03-27 11:21:22 -0400

    Adding function to api folder

---
+++ functions/api/forum.js
@@ -0,0 +1,12 @@
+// Function for retreiving post data from regular forum KV worker
+export async function onRequestGet({ env }) {
+    const allKeys = await env.COOLFROG_FORUM.list();
+    const allPosts = [];
+  
+    for (const key of allKeys.keys) {
+      const post = await env.COOLFROG_FORUM.get(key.name, { type: 'json' });
+      allPosts.push(post);
+    }
+  
+    return allPosts;
+  };
